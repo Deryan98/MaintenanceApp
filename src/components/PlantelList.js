f1df0448b3761abbb851ca5e5740755c3715e192
Deryan98 <00125116@uca.edu.sv>
2021-03-16 21:17:58 -0600

    Building dimanyc lists according to plantel type

--- src/components/PlantelList.js
+++ src/components/PlantelList.js
@@ -1,77 +1,46 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
-import { color } from "react-native-reanimated";
-import Colors from "../constants/Colors";
-import Dimensions from "../constants/Dimensions";
 
-export default function AnnounceScreen() {
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+export default function PlantelList({ data, tipoPlantel }) {
+  const renderItem = ({ item }) => <Item title={item.title} />;
+
+  let dataToRender = [];
+
+  data.map((plantel) => {
+    if (plantel.tipo == tipoPlantel) {
+      dataToRender.push(plantel);
+    }
+  });
+
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar hidden={true} barStyle="light-content" />
-      <View style={styles.body}>
-        <View style={styles.titleContainer}>
-          <Text style={styles.title}>App de Mantenimiento</Text>
-        </View>
-        <View style={styles.paragraphContainer}>
-          <Text style={styles.paragraph}>
-            Esta aplicación es un proyecto de Innovación empresarial
-          </Text>
-        </View>
-        <View style={styles.announcementContainer}>
-          <Text style={styles.announcement}>Espérala Pronto!</Text>
-        </View>
-      </View>
-    </SafeAreaView>
+    <View>
+      <FlatList
+        data={dataToRender}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Colors.dominant,
-    alignItems: "center",
-    justifyContent: "center",
   },
-  body: {
-    backgroundColor: Colors.complementary,
-    height: Dimensions.height / 3,
-    width: Dimensions.width / 1.2,
-    borderRadius: 15,
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
   title: {
-    color: Colors.dominant,
-    fontSize: 30,
-  },
-  titleContainer: {
-    backgroundColor: Colors.complementary,
-    borderWidth: 5,
-    borderColor: Colors.complementary,
-    borderTopStartRadius: 15,
-    borderTopEndRadius: 15,
-    justifyContent: "center",
-    alignItems: "center",
-    height: "30%",
-  },
-  paragraphContainer: {
-    height: "50%",
-    width: "100%",
-    borderRadius: 15,
-    borderColor: Colors.complementary,
-    borderWidth: 5,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  paragraph: { color: Colors.inactive, fontSize: 18, textAlign: "center" },
-  announcementContainer: {
-    height: "20%",
-    flexDirection: "column",
-  },
-  announcement: {
-    color: Colors.accent,
-    textAlign: "center",
-    alignItems: "center",
-    fontWeight: "bold",
-    fontSize: 18,
+    fontSize: 20,
   },
 });
--- src/screens/AssistanceScreen.js
+++ src/screens/AssistanceScreen.js
@@ -1,9 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
 import Colors from "../constants/Colors";
 import Dimensions from "../constants/Dimensions";
 import { ButtonGroup } from "react-native-elements";
+import PlantelList from "../components/PlantelList";
 
 export default function AssistanceScreen() {
   const [selectedIndex, setSelectedIndex] = useState(0);
@@ -21,55 +22,131 @@ export default function AssistanceScreen() {
     setSelectedIndex(selectedIndex);
   };
 
+  const DATA = [
+    {
+      id: "plantel1",
+      title: "KSA1 San Andrés",
+      image: "string1",
+      tipo: "Fijo",
+    },
+    {
+      id: "plantel2",
+      title: "KST1 Santa Tecla",
+      image: "string2",
+      tipo: "Fijo",
+    },
+    {
+      id: "plantel3",
+      title: "KSM1 San Miguel",
+      image: "string3",
+      tipo: "Fijo",
+    },
+    {
+      id: "plantel4",
+      title: "Plantel Sonsonate",
+      image: "string1",
+      tipo: "Mini",
+    },
+    {
+      id: "plantel5",
+      title: "Plantel La Paz",
+      image: "string2",
+      tipo: "Mini",
+    },
+    {
+      id: "plantel6",
+      title: "Plantel Apopa",
+      image: "string3",
+      tipo: "Mini",
+    },
+    {
+      id: "plantel7",
+      title: "Plantel Santa Ana",
+      image: "string3",
+      tipo: "Mini",
+    },
+    {
+      id: "plantel8",
+      title: "Plantel Santa Rosa de Lima",
+      image: "string3",
+      tipo: "Mini",
+    },
+    {
+      id: "plantel9",
+      title: "KM25",
+      image: "string1",
+      tipo: "Movil",
+    },
+    {
+      id: "plantel10",
+      title: "KM26",
+      image: "string2",
+      tipo: "Movil",
+    },
+    {
+      id: "plantel11",
+      title: "KM27",
+      image: "string3",
+      tipo: "Movil",
+    },
+    {
+      id: "plantel12",
+      title: "KM28",
+      image: "string3",
+      tipo: "Movil",
+    },
+    {
+      id: "plantel13",
+      title: "KM29",
+      image: "string3",
+      tipo: "Movil",
+    },
+    {
+      id: "plantel14",
+      title: "KM35",
+      image: "string3",
+      tipo: "Movil",
+    },
+  ];
+
   const renderFragment = (selectedIndex) => {
-    console.log(selectedIndex);
     switch (selectedIndex) {
       case 0:
-        return (
-          <View>
-            <Text>Planteles Fijos</Text>
-          </View>
-        );
+        return <PlantelList tipoPlantel="Fijo" data={DATA} />;
       case 1:
-        return (
-          <View>
-            <Text>Mini Planteles</Text>
-          </View>
-        );
+        return <PlantelList tipoPlantel="Mini" data={DATA} />;
       case 2:
-        return (
-          <View>
-            <Text>Planteles Móviles</Text>
-          </View>
-        );
+        return <PlantelList tipoPlantel="Movil" data={DATA} />;
     }
   };
 
   return (
     <SafeAreaView style={styles.mainContainer}>
       <StatusBar hidden={true} barStyle="light-content" />
-      <View style={styles.container}>
-        <View style={styles.titleContainer}>
-          <Text style={styles.title}>Control de Asistencia</Text>
-        </View>
-        <View style={styles.indicationContainer}>
-          <Text style={styles.indication}>
-            Elija un plantel para marcar asistencia
-          </Text>
-        </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>Control de Asistencia</Text>
+          </View>
+          <View style={styles.indicationContainer}>
+            <Text style={styles.indication}>
+              Elija un plantel para marcar asistencia
+            </Text>
+          </View>
 
-        <ButtonGroup
-          onPress={updateIndex}
-          selectedIndex={selectedIndex}
-          buttons={buttons}
-          containerStyle={styles.fragmentContainer}
-          innerBorderStyle={{ width: 0 }}
-          buttonContainerStyle={styles.fragmentButtonContainer}
-          selectedTextStyle={{ color: Colors.accent }}
-          selectedButtonStyle={styles.fragmentSelectedButton}
-        />
-        {renderFragment(selectedIndex)}
-      </View>
+          <ButtonGroup
+            onPress={updateIndex}
+            selectedIndex={selectedIndex}
+            buttons={buttons}
+            containerStyle={styles.fragmentContainer}
+            innerBorderStyle={{ width: 0 }}
+            buttonContainerStyle={styles.fragmentButtonContainer}
+            selectedTextStyle={{ color: Colors.accent }}
+            selectedButtonStyle={styles.fragmentSelectedButton}
+          />
+          {renderFragment(selectedIndex)}
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
